60e95d981afed547d755ac31d27387e5fd9adb17
Lucashcr <lucash.rocha@hotmail.com>
2022-05-18 00:59:56 -0300

    Associa usuário ao evento (ForeignKey)

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from core.models import Event
 
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'user', 'event_date', 'created_at')
+    # list_filter = ('event_date',)
+    # search_fields = ('title', 'description')
+
 # Register your models here.
-admin.site.register(Event)
+admin.site.register(Event, EventAdmin)
--- core/models.py
+++ core/models.py
@@ -1,8 +1,15 @@
 from django.db import models
+from django.utils.timezone import now
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Event(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
-    event_date = models.DateTimeField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(verbose_name="Título do evento", max_length=100)
+    description = models.TextField(verbose_name="Descrição", blank=True, null=True)
+    event_date = models.DateTimeField(verbose_name="Data marcada", default=now)
+    created_at = models.DateTimeField(verbose_name="Data de criação", auto_now_add=True)
+    user = models.ForeignKey(User, verbose_name="Usuário", on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
