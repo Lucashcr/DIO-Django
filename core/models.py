54553febe73b56f1a6d2d3f075c5bf25e978b3f5
Lucashcr <lucash.rocha@hotmail.com>
2022-05-17 23:27:45 -0300

    Cria o modelo Evento

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    event_date = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
